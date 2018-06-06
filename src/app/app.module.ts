878cf0b9ae55e01d9ab034d2c874fecc1b760495
atimoshevsky <alexander.timoshevsky@gmail.com>
2018-06-06 22:10:55 +0200

    Adding material and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { AppBlockComponent } from './block/app-block.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,20 @@ import { AppBlockComponent } from './block/app-block.component';
     AppBlockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    RouterModule.forRoot([
+      { path: 'block', component: AppBlockComponent},
+      { path: 'hash', component: AppBlockComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
