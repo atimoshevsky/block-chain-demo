6d41bba2349022852dd573ec52ccbb7498c7621d
atimoshevsky <alexander.timoshevsky@gmail.com>
2018-06-11 23:31:32 +0200

    Block implementation

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -9,12 +9,36 @@ import * as CryptoJS from 'crypto-js';
 })
 export class BlockComponent implements OnInit {
   userData = '';
+  hash = '';
+  nonce = 252196;
   constructor() { }
 
   ngOnInit() {
+    this.hash = this.getSHA256(252196);
   }
 
-  getSHA256(): string {
-    return CryptoJS.SHA256(this.userData).toString();
+  getSHA256(nonce: number): string {
+    return CryptoJS.SHA256('11/06/2018' + JSON.stringify(this.userData) + nonce).toString();
+  }
+
+  onMine(): void {
+    this.mineBlock(4);
+  }
+
+  userDataChange() {
+   this.hash = this.getSHA256(0);
+   this.nonce = 0;
+  }
+
+  mineBlock(difficulty: number) {
+    let nonce = 0;
+    let hash = this.getSHA256(nonce);
+
+    while (hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+      nonce++;
+      hash = this.getSHA256(nonce);
+    }
+    this.hash = hash;
+    this.nonce = nonce;
   }
 }
