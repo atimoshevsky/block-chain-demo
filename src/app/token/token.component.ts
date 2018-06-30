58931be0c8747945974ded3ac36f13ea8d8a2ac7
atimoshevsky <alexander.timoshevsky@gmail.com>
2018-06-30 08:46:14 +0200

    table stiling

--- src/app/token/token.component.ts
+++ src/app/token/token.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlockTransaction } from '../../core/block-transaction';
 
 @Component({
   selector: 'app-token',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./token.component.css']
 })
 export class TokenComponent implements OnInit {
-
+  blockTransaction: BlockTransaction[] = [
+    new BlockTransaction(1, 100, 'Alex', 'Vika'),
+    new BlockTransaction(2, 30, 'Vika', 'Mama'),
+    new BlockTransaction(3, 30, 'Vika', 'Papa')
+];
   constructor() { }
 
   ngOnInit() {
--- src/core/block-transaction.ts
+++ src/core/block-transaction.ts
@@ -3,4 +3,11 @@ export class BlockTransaction {
     amount: number;
     from: string;
     to: string;
+
+    constructor(id: number, amount: number, from: string, to: string) {
+        this.id = id;
+        this.amount = amount;
+        this.from = from;
+        this.to = to;
+    }
 }
