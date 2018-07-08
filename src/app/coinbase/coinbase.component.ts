42c3a5f3eaeb3dfd02f65973e912dcdf0a726843
atimoshevsky <alexander.timoshevsky@gmail.com>
2018-07-08 13:06:55 +0200

    coin base finalization

--- src/app/coinbase/coinbase.component.ts
+++ src/app/coinbase/coinbase.component.ts
@@ -9,31 +9,30 @@ import { Mode } from '../../core/mode';
 })
 export class CoinbaseComponent implements OnInit {
   mode: Mode =  Mode.CoinBase;
-  tokenInitalNonce: Array<number> = [48623, 209816, 91548, 12577, 3068];
-  tokenInitalAmounts: Array<number> = [555.55, 250.15, 50.45, 40.32, 10.19,
-    15.01, 10.20, 14.34, 10.23, 10.11,
-    12.54, 11.12, 16.43, 30.12, 20.43,
-    10.12, 5.43, 4.54, 7.23, 9.54,
-    4.12, 3.65, 1.45, 3.23, 4.34
+  tokenInitalNonce: Array<number> = [47316, 32491, 112451, 28611, 13739];
+  tokenInitalAmounts: Array<number> = [50, 15, 15, 5, 5,
+    15, 10, 14, 10, 1,
+    1, 1, 1, 3, 2,
+    10, 0.5, 0.4, 0.1, 0.95
   ];
   tokenInitalNames: Array<Array<string>> = [
-    ['Credit Suisse', 'Alex'],
     ['Alex', 'Vika'],
-    ['Alex', 'Michal'],
+    ['Alex', 'Misha'],
     ['Alex', 'Varvara'],
+    ['Alex', 'Oleksii'],
     ['Alex', 'Oleksii'],
     ['Vika', 'Lida'],
     ['Vika', 'Valera'],
     ['Vika', 'Tania'],
     ['Vika', 'Michal'],
-    ['Vika', 'Nadia'],
+    ['Misha', 'Oleksii'],
     ['Alex', 'Wojciech'],
-    ['Wojciech', 'Lukasz'],
-    ['Lukasz', 'Tomasz'],
-    ['Tomasz', 'Alex'],
-    ['Gerry', 'Toby'],
+    ['Alex', 'Lukasz'],
+    ['Alex', 'Tomasz'],
+    ['Alex', 'Alex'],
+    ['Alex', 'Toby'],
     ['Toby', 'Simon'],
-    ['Simon', 'Tim'],
+    ['Wojciech', 'Tim'],
     ['Tim', 'Colin'],
     ['Colin', 'Robert'],
     ['Robert', 'Gerry'],
